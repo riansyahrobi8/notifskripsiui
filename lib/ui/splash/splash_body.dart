0676cf7ada54e9ad169d2192907046e6ca11b939
riansyahrobi8 <robidahariansyah8@gmail.com>
2021-04-10 10:09:38 +0700

    splash done

---
+++ lib/ui/splash/splash_body.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:notifskripsiui/utils/constanta_colors.dart';
+import 'package:notifskripsiui/utils/size_config.dart';
+
+class SplashBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          top: -72.0,
+          right: -120.0,
+          child: Container(
+            width: getProportionateScreenWidth(280.0),
+            height: getProportionateScreenWidth(280.0),
+            decoration:
+                BoxDecoration(color: primaryColor, shape: BoxShape.circle),
+          ),
+        ),
+        Center(
+          child: Text(
+            "My App",
+            style: TextStyle(fontSize: 44.0, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Positioned(
+          left: -72.0,
+          bottom: -120.0,
+          child: Container(
+            width: getProportionateScreenWidth(280.0),
+            height: getProportionateScreenWidth(280.0),
+            decoration:
+                BoxDecoration(color: primaryColor, shape: BoxShape.circle),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,12 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:notifskripsiui/routing/constanta_route.dart';
+import 'package:notifskripsiui/ui/splash/splash_body.dart';
+import 'package:notifskripsiui/utils/size_config.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final int splashDelay = 4;
+
+  void navigateToPage() {
+    Navigator.pushReplacementNamed(context, login);
+  }
+
+  Timer _loadWidget() {
+    var _duration = Duration(seconds: splashDelay);
+    return Timer(_duration, navigateToPage);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadWidget();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      body: Center(
-        child: Text("Splash"),
-      ),
+      body: SplashBody(),
     );
   }
 }
