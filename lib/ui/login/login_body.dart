85e9e79712bd5088f1157fa8149b8e22093dbe09
Robi Dahariansyah <robidahariansyah8@gmail.com>
2021-05-31 10:33:51 +0700

    change user access

--- lib/ui/login/login_body.dart
+++ lib/ui/login/login_body.dart
@@ -6,7 +6,7 @@ import 'package:notifskripsiui/utils/constanta_strings.dart';
 import 'package:notifskripsiui/utils/size_config.dart';
 
 class LoginBody extends StatelessWidget {
-  final String user = "Adin";
+  final String user = "Admin";
 
   @override
   Widget build(BuildContext context) {
